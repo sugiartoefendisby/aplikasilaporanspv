b16ba40a41e56950af194e5028091db6388ed1c1
Sugiarto Efendi <157724481+sugiartoefendisby@users.noreply.github.com>
2025-08-14 02:13:07 +0700

    2nd commit

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "laporan-spv-v1";
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/report-form.html',
+  '/login-style.css',
+  '/report-style.css',
+  '/login.js',
+  '/report.js',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
